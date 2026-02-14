0dca921784cf20bcab5dd360815ebc719a7d3e5c
YuvrajGora <yuvrajgora10mar@gmail.com>
2026-02-14 12:56:37 +0530

    Fix: Use process.env.PORT for Render deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import goalRoutes from './routes/goals.js';
 import recurringRoutes from './routes/recurring.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
